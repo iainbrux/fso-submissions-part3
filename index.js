84314e8ed303f85c2fcbb754ebf85421d60649b0
iainbrux <me@iainbrux.com>
2021-06-06 09:40:53 +0100

    3.1 completion

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log('Hello World')
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+let contacts = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendick",
+    number: "39-23-6423122",
+  },
+];
+
+app.get('/', (request, response) => {
+    response.send('<h1>Contacts list</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+    console.log(contacts)
+    response.json(contacts)
+})
+
+const PORT = 3001
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
