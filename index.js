340cf4feba9de157602348d425e08a6ca8f0cdb7
Iain Brookes <iain.brookes@outlook.com>
2021-06-06 23:17:11 +0100

    allow CORS

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const morgan = require('morgan');
+const cors = require('cors')
 
 const app = express();
 
 app.use(express.json());
 app.use(express.static('build'));
+app.use(cors())
 
 let contacts = [
   {
