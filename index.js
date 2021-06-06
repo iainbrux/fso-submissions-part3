a9087d9c08181f6809d036d895e342c912fd0cde
Iain Brookes <iain.brookes@outlook.com>
2021-06-06 23:00:44 +0100

    upload files to heroku

--- index.js
+++ index.js
@@ -102,5 +102,5 @@ app.delete("/api/persons/:id", (request, response) => {
   console.log(`Contact with id ${id} has been deleted`);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
