44c7398068a638ac7f2fcb99ff2c3e2c7bcb5f26
iainbrux <me@iainbrux.com>
2021-06-07 21:21:47 +0100

    3.12 mongo.js file for prep

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const password = process.argv[2];
+
+const dbURI = `mongodb+srv://fullstack:${password}@cluster0.kgfql.mongodb.net/phonebook-app?retryWrites=true&w=majority`;
+
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const contactSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+});
+
+const Contact = mongoose.model("Contact", contactSchema);
+
+const contact = new Contact({
+  name: process.argv[3],
+  number: process.argv[4],
+});
+
+if (process.argv[3] && process.argv[4]) {
+  contact.save().then((result) => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else if (!process.argv[3] && !process.argv[4]) {
+  console.log('phonebook:')
+  Contact.find({}).then((contacts) => {
+    contacts.forEach((contact) => {
+      console.log(`${contact.name} ${contact.number}`);
+      mongoose.connection.close();
+    });
+  });
+} else {
+    console.log('A full contact has not been specified.')
+    console.log('Please ensure a name and number have been entered.')
+    return process.exit(1);
+}
